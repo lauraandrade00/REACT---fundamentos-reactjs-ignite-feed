814313d78d8c3c705956255dc46f2515a9d14dc4
Laura Andrade <lauraandradecamp@gmail.com>
2023-03-25 16:42:01 -0300

    Finishing post styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,7 +18,11 @@ export function Post() {
                 <p>Fala galeraa</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat.</p>
                 <p><a href="">laura.design/doctorcare</a></p>
-                <p><a href="">#novoprojeto #nlw #rocketseat</a></p> 
+                <p>
+                    <a href=''>#novoprojeto</a>{' '}
+                    <a href=''>#nlw</a>{' '}
+                    <a href=''>#rocketseat</a>
+                </p> 
             </div>
         </article>
     );
