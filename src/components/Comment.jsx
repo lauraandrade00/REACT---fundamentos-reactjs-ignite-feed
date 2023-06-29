98dcb91cc97e85afb8425275b05069ac684b627e
Laura Andrade <lauraandradecamp@gmail.com>
2023-06-29 17:37:42 -0300

    manipulating comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
+import { useState } from 'react';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(state => {
+            return state + 1
+        });
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src='https://github.com/lauraandrade00.png' alt=''/>
@@ -15,7 +28,7 @@ export function Comment({ content }) {
                             <time title='22 de março às 17:34h'dateTime='2023-03-22 17:34:21'>Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24} />
                         </button>
                     </header>
@@ -24,9 +37,9 @@ export function Comment({ content }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
